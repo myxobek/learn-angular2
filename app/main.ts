4b943f1dfb19ec050b19a335b627f45f21241335
myxobek <myxobek@gmail.com>
2016-09-26 14:24:02 +0300

    completed initial setup

---
+++ app/main.ts
@@ -0,0 +1,16 @@
+/// <reference path='../node_modules/angular2/ts/typings/node/node.d.ts'/>
+/// <reference path='../node_modules/angular2/typings/browser.d.ts'/>
+
+import { Component } from 'angular2/core';
+import { bootstrap } from 'angular2/platform/browser';
+
+@Component({
+    selector: 'todo-app',
+    template: '<h1>TODO!!</h1>'
+})
+
+class AppComponent {
+
+}
+
+bootstrap( AppComponent );
