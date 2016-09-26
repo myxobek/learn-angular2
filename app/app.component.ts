07cddff73425b7e38efc47109a3a092cc745174d
myxobek <myxobek@gmail.com>
2016-09-26 15:46:23 +0300

    Quickstart: step 6-7

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My first Angular2 App</h1>'
+    template: '<h1>My Second Angular2 App</h1>'
 })
 
 export class AppComponent {}
