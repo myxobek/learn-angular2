0adb1710fe23fe28f5c8fc291bb182ed96075b8d
myxobek <myxobek@gmail.com>
2016-09-26 21:12:40 +0300

    Tutorial: Routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,41 +1,18 @@
-import { Component,OnInit } from '@angular/core';
-
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
     template: `
         <h1>{{title}}</h1>
-        <h2>My Heroes</h2>
-        <ul class="heroes">
-            <li *ngFor="let hero of heroes" 
-                (click)="onSelect(hero)"
-                [class.selected] = "hero === selectedHero"
-            >
-                <span class="badge">{{hero.id}}</span> {{hero.name}}
-            </li>
-        </ul>
-        <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+        <nav>
+            <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+            <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+        </nav>
+        <router-outlet></router-outlet>
     `,
+    styleUrls: ['app/app.component.css']
 })
 
-export class AppComponent implements OnInit{
+export class AppComponent {
     title:string = 'Tour of Heroes';
-    heroes:Hero[];
-    selectedHero: Hero;
-
-    constructor(private heroService: HeroService){}
-
-    getHeroes(): void{
-        this.heroService.getHeroesSlowly().then( heroes => this.heroes = heroes );
-    }
-
-    ngOnInit(): void {
-        this.getHeroes();
-    }
-
-    onSelect( hero:Hero ):void {
-        this.selectedHero = hero;
-    };
 }
