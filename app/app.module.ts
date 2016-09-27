094eb6841558769a4a8748ce4b6115fdd78dd41d
myxobek <myxobek@gmail.com>
2016-09-27 20:34:14 +0300

    Tutorial: crud

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule }         from '@angular/core';
 import { BrowserModule }    from '@angular/platform-browser';
 import { FormsModule }       from '@angular/forms';
+import { HttpModule }       from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent }     from './app.component';
 import { HeroesComponent }     from './heroes.component';
@@ -14,7 +19,9 @@ import { routing } from './app.routing'
     imports: [
         BrowserModule,
         FormsModule,
-        routing
+        routing,
+        HttpModule,
+        InMemoryWebApiModule.forRoot( InMemoryDataService )
     ],
     declarations: [
         AppComponent,
